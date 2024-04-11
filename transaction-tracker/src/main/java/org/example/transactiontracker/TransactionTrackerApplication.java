6eda2592dc68eee18c978a734ed534146bedb7cd
Ivan <ivanmalcev1233@gmail.com>
2024-04-11 15:16:07 +0300

    add base project

---
+++ transaction-tracker/src/main/java/org/example/transactiontracker/TransactionTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.example.transactiontracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionTrackerApplication.class, args);
+	}
+
+}
---
+++ transaction-tracker/src/test/java/org/example/transactiontracker/TransactionTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.transactiontracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

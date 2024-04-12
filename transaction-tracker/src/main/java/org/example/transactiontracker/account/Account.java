0ab62f3c718e0407972758b14d4136e0e591b498
Ivan <ivanmalcev1233@gmail.com>
2024-04-13 02:01:10 +0300

    add Account, Transaction model

---
+++ transaction-tracker/src/main/java/org/example/transactiontracker/account/Account.java
@@ -0,0 +1,20 @@
+package org.example.transactiontracker.account;
+
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "accounts")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "account_number", nullable = false, unique = true)
+    private String accountNumber;
+
+    @Column(name = "total_amount", nullable = false)
+    private BigDecimal totalAmount;
+}
